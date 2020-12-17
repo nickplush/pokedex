9e8f74d4ef60357290103939ffbf753c6d213e88
nickp <nikplushenko.exceedteam@gmail.com>
2020-12-17 16:48:31 +0300

    add selector

--- src/actions/selectorActions.js
+++ src/actions/selectorActions.js
@@ -1,4 +1,12 @@
+import axios from 'axios'
+
 export const changeSelector = (type) => (dispatch) => {
-  console.log('LOOOG', type)
   dispatch({ type: 'CHANGE_SELECTOR', payload: type })
+}
+export const removeSelector = () => (dispatch) => {
+  dispatch({ type: 'REMOVE_SELECTOR', payload: false })
+}
+export const getTypes = async () => {
+  const typesArr = await axios.get('https://pokeapi.co/api/v2/type')
+  return (typesArr.data.results)
 }
--- src/components/Lib/Table.js
+++ src/components/Lib/Table.js
@@ -3,13 +3,17 @@ import Grid from '@material-ui/core/Grid'
 import TableCard from './TableCard'
 import { Paginator } from './Paginator'
 import { array, bool } from 'prop-types'
+import Selector from './Selector'
+import { useSelector } from 'react-redux'
 
 const Table = ({ pokemons, display }) => {
+  const types = useSelector(state => state.selector)
   return (
     <div>
-      {Boolean(display) &&
+      {Boolean(display && types === 'all') &&
         <Paginator/>
       }
+      <Selector/>
         <Grid container spacing={12}
               justify="flex-start"
               alignItems="center">
